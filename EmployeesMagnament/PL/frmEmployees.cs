d2e6fa881670b8f136cd7d4d5b4b686bc2164fec
jeftounz <srjesusc@gmail.com>
2023-08-02 21:08:33 -0400

    Agregar archivos de proyecto.

---
+++ EmployeesMagnament/PL/frmEmployees.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeesMagnament.PL
+{
+    public partial class frmEmployees : Form
+    {
+        public frmEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmDepartments_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
